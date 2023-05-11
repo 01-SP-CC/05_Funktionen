d3ce0d35fd777a1d83c28bb9b6fdd9fc5817e8e4
sven-pke <sven-perschke@gmx.de>
2023-05-11 10:16:41 +0200

    Parametrisierung + Datenübergabe von INNEN

--- script_01.js
+++ script_01.js
@@ -3,3 +3,30 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo Karsten!");
+// }
+
+// test(); // Mehrfaches Aufrufen möglich.
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen()
+
+{   
+    // what happens in VEGAS ...
+    let firstName = "Sven"; 
+    console.log("Hallo " +  firstName + "!");
+
+}
+
+// console.log(firstName); // Fehler: Scope!
