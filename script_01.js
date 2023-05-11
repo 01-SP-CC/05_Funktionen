a9ac3aa2562779d7017042d186977b67a092df7b
sven-pke <sven-perschke@gmx.de>
2023-05-11 11:52:47 +0200

    Vorbereitung

--- script_01.js
+++ script_01.js
@@ -47,10 +47,28 @@
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter
 
-ausgabeNamenParams("Dave", "Batista");
-ausgabeNamenParams("Jamshid", "Rahmanzani");
-ausgabeNamenParams(prompt("Vorname?"), prompt("Nachname?"))
+// ausgabeNamenParams("Dave", "Batista");
+// ausgabeNamenParams("Jamshid", "Rahmanzani");
+// ausgabeNamenParams(prompt("Vorname?"), prompt("Nachname?"))
 
-function ausgabeNamenParams(firstName, familyName) {
-    console.log("Hallo " +  firstName + " " + familyName + "!");
+// function ausgabeNamenParams(firstName, familyName) {
+//     console.log("Hallo " +  firstName + " " + familyName + "!");
+// }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob) --> Begründer: Cecil Martin aka Uncle Bob (Nice to Know)
+// SRP single responsibility principle
+
+ausgabeNamenParams2("Jamshid", "Rahmanzani");
+
+function ausgabeNamenParams2(firstName, familyName) {
+
+     // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    
+     // 2. Funktionalität: string output
+    console.log(outputStr);
 }
+
